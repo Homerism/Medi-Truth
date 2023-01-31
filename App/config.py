bc67947dc6102e13c3668118a039f9816d45f6bd
JDHomere <jahmel.homer@my.uwi.edu>
2023-01-31 02:52:52 +0000

    Config update

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:mydb_fpx2_user:jaiibUNVQ0ShPVOGjkbFOdRoAhQMM8eL@dpg-cfc4t15a4998vdd76ugg-a.oregon-postgres.render.com/mydb_fpx2"
+SQLALCHEMY_DATABASE_URI = "postgres://mydb_fpx2_user:jaiibUNVQ0ShPVOGjkbFOdRoAhQMM8eL@dpg-cfc4t15a4998vdd76ugg-a.oregon-postgres.render.com/mydb_fpx2"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "PRODUCTION"
