c2e216c744c59e519eeb241e45d4adb04d610d9c
JDHomere <jahmel.homer@my.uwi.edu>
2023-02-17 10:42:18 -0400

    init update

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -2,5 +2,6 @@ from .user import user_views
 from .index import index_views
 from .login import login_views
 from .profile import profile_views
+from .signup import signup_views
 
-views = [user_views, index_views, login_views, profile_views]
+views = [user_views, index_views, login_views, signup_views, profile_views]
