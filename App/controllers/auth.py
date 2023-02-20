0c2eb6bf195bed131ec4c485f8bd98891bf55bb1
JDHomere <jahmel.homer@my.uwi.edu>
2023-02-19 21:45:09 -0400

    authenticate update

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -3,7 +3,7 @@ from flask_jwt import JWT
 from App.models import User
 
 
-def authenticate(username, password):
+def authenticate(username, type, password):
     user = User.query.filter_by(username=username).first()
     if user and user.check_password(password):
         return user
