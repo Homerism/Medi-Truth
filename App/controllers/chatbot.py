096103f1f50aa6125c2cca8359d05da9fc42fc8a
JDHomere <jahmel.homer@my.uwi.edu>
2023-03-17 19:07:29 -0400

    update

--- App/controllers/chatbot.py
+++ App/controllers/chatbot.py
@@ -35,7 +35,6 @@ def call_until_return_response(func, history, input):
         result = func(history,input)
     return result
 
-
 def check_user_input(user_input):
     health_keywords = ['covid','coronavirus','health', 'medicine', 'doctor', 'hospital', 'sickness', 'symptom', 'treatment', 'disease', 'injury', 'illness',"abdomen",
                        "abdominal","acupuncture","ADD","addiction","ADHD","adolescent","epilepsy","eye","family","feet","bipolar","blood","bone","brain","breast",
@@ -48,7 +47,19 @@ def check_user_input(user_input):
                        "medical","medicine","menopause","mental","migraine","mobility","MRI","neonatal","neurology","nutrition","obesity","obstetrics","oncology",
                        "ophthalmology","optometry","oral","orthodontics","orthopedic","osteoporosis","ovarian","pain","palliative","pediatric","pharmacy",
                        "physical","plastic","podiatry","pregnancy","psychiatry","psychology","radiation","radiology","rehabilitation","reproductive","respiratory",
-                       "rheumatoid","rheumatology","safety","senior","sexual","sickle","skin","sleep","speech","spinal","sports","stroke","surgery","therapist","therapy",   
+                       "rheumatoid","rheumatology","safety","senior","sexual","sickle","skin","sleep","speech","spinal","sports","stroke","surgery","therapist","therapy",
+                       "water","compound","chemical","diseases","cure","COVID-19", "Vaccination", "Coronavirus", "Flu", "Cancer", "Diabetes", "Heart disease", "HIV/AIDS", 
+                       "Alzheimer's disease", "Asthma", "Arthritis", "Depression", "Obesity", "Migraine", "High blood pressure", "Stroke", "Pregnancy", "Menopause", "Erectile dysfunction", 
+                       "Cholesterol", "Acne", "Allergies", "Anemia", "Anxiety", "Back pain", "Bipolar disorder", "Blood pressure", "Bronchitis", "Celiac disease", "Chickenpox", "Chlamydia", 
+                       "Chronic fatigue syndrome", "Cold sores", "Colitis", "Conjunctivitis", "Constipation", "COPD", "Crohn's disease", "Cystic fibrosis", "Dementia", "Dental health", "Dermatitis", 
+                       "Diarrhea", "Digestive system", "Eating disorders", "Eczema", "Endometriosis", "Epilepsy", "Fibromyalgia", "Gallstones", "Gout", "Headaches", "Hemorrhoids", "Hepatitis", "Herpes", 
+                       "Hyperthyroidism", "Hypothyroidism", "Incontinence", "Influenza", "Insomnia", "Irritable bowel syndrome","IBS","Jaundice", "Kidney disease", "Lactose intolerance", "Leukemia", 
+                       "Lupus", "Lyme disease", "Macular degeneration", "Malaria", "Melanoma", "Menstrual cycle", "Mesothelioma", "Mumps", "Multiple sclerosis","MS", "Myasthenia gravis", "Nasal congestion",
+                       "Neck pain", "Nephrotic syndrome", "Neuropathy", "Osteoporosis", "Ovarian cancer", "Overactive bladder", "Pancreatic cancer", "Panic attacks", "Parkinson's disease", "Pneumonia", "Polio", 
+                       "Polycystic ovary syndrome","PCOS","Psoriasis", "Raynaud's disease", "Restless leg syndrome", "Rheumatoid arthritis", "Rosacea", "Sarcoidosis", "Scabies", "Schizophrenia", "Sciatica", 
+                       "Scleroderma", "Shingles", "Sinusitis", "Sleep apnea", "Sore throat", "Stomach ulcers", "Stress", "Sunburn", "Swine flu", "Syphilis", "Tinnitus", "Tonsillitis", "Tuberculosis","TB", "Ulcerative colitis", 
+                       "Urinary tract infection","UTI", "Varicose veins", "Vertigo", "Vitamin deficiency", "Warts", "West Nile virus", "Whooping cough", "Yeast infection", "Zika virus", "Abdominal pain", 
+                       "Acute respiratory distress syndrome","ARDS", "Adenoids", "ADHD", "Adrenal gland disorders", "Alcoholism", "Allergic","pain","syndrome","ache","infection"
                        ]
     is_health_related = any(keyword in user_input.lower() for keyword in health_keywords)
     if is_health_related:
--- App/forms/reaction.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectMultipleField
-from wtforms.validators import InputRequired
-
-class DoctorReaction(FlaskForm):
-    reaction = SelectMultipleField('Do You Approve or Disapprove of this article', coerce=str, choices=[('Approve','Disapprove')])
