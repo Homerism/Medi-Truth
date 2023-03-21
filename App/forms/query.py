8fa3e54e2401205333e3824f25e9ec3e258be43f
KareemWilliams20 <brwncreative2021@gmail.com>
2023-03-21 17:31:33 -0400

    Most Recent UI changes (Index, Query and About Us)

--- App/forms/query.py
+++ App/forms/query.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import TextAreaField, SubmitField, StringField
 from wtforms.validators import InputRequired
 
+
 class QueryForm(FlaskForm):
-    textarea = TextAreaField('Text', render_kw={"rows": 100, "cols": 100}, validators=[InputRequired()])
-    submit = SubmitField('Submit', render_kw={'class': 'btn amber darken-4 waves-effect waves-light'})
+    textarea = StringField(
+        'Text', render_kw={"rows": 100, "cols": 100}, validators=[InputRequired()])
+    submit = SubmitField('Submit', render_kw={
+                         'class': 'btn amber darken-4 waves-effect waves-light'})
