5c26060d17ba237297235b1e0dd9136b44d8c3fb
KareemWilliams20 <brwncreative2021@gmail.com>
2023-04-11 11:31:24 -0400

    Front end changes

--- App/views/login.py
+++ App/views/login.py
@@ -1,32 +1,33 @@
-from flask import Blueprint, redirect, render_template, flash, url_for
-from flask_login import login_user, login_required, logout_user 
+from flask import Blueprint, redirect, render_template, flash, request, url_for
+from flask_login import login_user, login_required, logout_user
 from App.database import db
 from App.forms.login import LogIn
 from App.models import User
 
-login_views = Blueprint('login_views', __name__, template_folder='../templates')
+login_views = Blueprint('login_views', __name__,
+                        template_folder='../templates')
+
 
 @login_views.route('/login', methods=['GET'])
 def loginIndex():
-  form = LogIn()
-  return render_template('login.html', form=form)
+    return render_template('login.html')
+
 
 @login_views.route('/login', methods=['POST'])
 def loginAction():
-  form = LogIn()
-  error = ""
-  if form.validate:
-    user = User.query.filter_by(username=form.username.data).first()
-    if user and user.check_password(form.password.data):
-      login_user(user)
-      flash(f"User {form.username.data} was logged in successfully!")
-      return redirect(url_for('query_views.index_page'))
+    username = request.form.get("username")
+    password = request.form.get("password")
+    error = ""
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        login_user(user)
+        return redirect(url_for('query_views.index_page'))
     error = "Incorrect Password Input"
-    return render_template('login.html', form=form, error=error)
+    return render_template('login.html', error=error)
+
 
 @login_views.route('/logout', methods=['GET'])
 @login_required
 def logout():
-  flash(f"Goodbye...you was logged out.")
-  logout_user()
-  return redirect(url_for('index_views.index'))
+    logout_user()
+    return redirect(url_for('index_views.index'))
--- App/views/signup.py
+++ App/views/signup.py
@@ -1,56 +1,57 @@
-from flask import Blueprint, redirect, render_template, flash, url_for
+from flask import Blueprint, redirect, render_template, flash, request, url_for
 from App.database import db
 from App.forms.usersignup import UserSignUp
 from App.forms.doctorsignup import DoctorSignUp
 
 from App.controllers import (
-    create_user, 
+    create_user,
     get_npi_number,
     check_npi
 )
 
-signup_views = Blueprint('signup_views', __name__, template_folder='../templates')
+signup_views = Blueprint('signup_views', __name__,
+                         template_folder='../templates')
 
-#<----------------------User SIGNUP----------------------------------->
+# <----------------------User SIGNUP----------------------------------->
+
 
 @signup_views.route('/usersignup', methods=['GET'])
 def UserSignupIndex():
-  form = UserSignUp()
-  return render_template('usersignup.html', form=form)
+    return render_template('usersignup.html')
+
 
 @signup_views.route('/usersignup', methods=['POST'])
 def UserSignupAction():
-    form = UserSignUp()
+    username = request.form.get("username")
+    password = request.form.get("password")
     error = ""
-    if form.validate:
-       create_user(form.username.data, 'General User', form.password.data)
-       flash(f"User {form.username.data} created!")
-       return redirect(url_for('login_views.loginIndex'))
-    flash(f"User {form.username.data} was not created successfully!")
-    return render_template('usersignup.html', form=form)
+    create_user(username, 'General User', password)
+    return redirect(url_for('login_views.loginIndex'))
 
-#<----------------------DOCTOR SIGNUP----------------------------------->
+# <----------------------DOCTOR SIGNUP----------------------------------->
+
+
 @signup_views.route('/doctorsignup', methods=['GET'])
 def DoctorSignupIndex():
-  form = DoctorSignUp()
-  return render_template('doctorsignup.html', form=form)
+    form = DoctorSignUp()
+    return render_template('doctorsignup.html', form=form)
+
 
 @signup_views.route('/doctorsignup', methods=['POST'])
 def DoctorSignupAction():
     form = DoctorSignUp()
     error = ""
     if form.validate:
-       real_npi = check_npi(form.npi.data)
-       if not real_npi:
-          error = "This is an invalid NPI number"
-          return render_template('doctorsignup.html', form=form, error=error)
-       
-       npi = get_npi_number(form.npi.data)
-       if not npi:
-          error = "This is an invalid NPI number"
-          return render_template('doctorsignup.html', form=form, error=error)
-       
-       create_user(form.username.data, 'Doctor', form.password.data)
-       flash(f"User {form.username.data} created!")
-       return redirect(url_for('login_views.loginIndex'))
-    
+        real_npi = check_npi(form.npi.data)
+        if not real_npi:
+            error = "This is an invalid NPI number"
+            return render_template('doctorsignup.html', form=form, error=error)
+
+        npi = get_npi_number(form.npi.data)
+        if not npi:
+            error = "This is an invalid NPI number"
+            return render_template('doctorsignup.html', form=form, error=error)
+
+        create_user(form.username.data, 'Doctor', form.password.data)
+        flash(f"User {form.username.data} created!")
+        return redirect(url_for('login_views.loginIndex'))
