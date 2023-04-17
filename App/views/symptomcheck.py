5063b0eb1b9654502ff880dd8aa51e4fdd062d40
KareemWilliams20 <brwncreative2021@gmail.com>
2023-04-17 17:56:37 -0400

    Guest and User UI/UX updates

--- App/views/symptomcheck.py
+++ App/views/symptomcheck.py
@@ -1,20 +1,23 @@
 from flask import Blueprint, render_template, request, flash
 from flask_login import login_required
 
-symptom_views = Blueprint('symptom_views', __name__, template_folder='../templates')
+symptom_views = Blueprint('symptom_views', __name__,
+                          template_folder='../templates')
+
 
 @symptom_views.route('/symptoms', methods=['GET'])
 @login_required
 def index():
     return render_template('results.html')
 
+
 @symptom_views.route('/symptoms', methods=['POST'])
 @login_required
 def get_symptom_diagnosis():
     symptom_to_condition = {
-        "fever": ["flu", "COVID-19", "pneumonia",'Influenza', 'Malaria'],
-        "cough": ["common cold", "bronchitis", "COVID-19",'Pneumonia', 'Asthma'],
-        "headache": ["migraine", "tension headache", "sinusitis",'Cluster headache'],
+        "fever": ["flu", "COVID-19", "pneumonia", 'Influenza', 'Malaria'],
+        "cough": ["common cold", "bronchitis", "COVID-19", 'Pneumonia', 'Asthma'],
+        "headache": ["migraine", "tension headache", "sinusitis", 'Cluster headache'],
         "nausea": ["food poisoning", "gastroenteritis", "migraine"],
         "fatigue": ["anemia", "hypothyroidism", "sleep apnea"],
         'vomiting': ['Gastroenteritis', 'Food poisoning', 'Appendicitis'],
@@ -59,21 +62,20 @@ def get_symptom_diagnosis():
         "hair loss": ["Androgenic alopecia", "Telogen effluvium", "Alopecia areata"],
         "heart palpitations": ["Arrhythmia", "Atrial fibrillation", "Anxiety", "Hyperthyroidism"],
         "high blood pressure": ["Hypertension", "Atherosclerosis", "Heart attack", "Stroke"]
-}
-    
+    }
+
     symptoms = str(request.form["symptoms"])
-    symptoms = symptoms.replace(", ",",")
+    symptoms = symptoms.replace(", ", ",")
     symptoms = symptoms.strip().lower().split(",")
     potential_conditions = []
-    
+
     for symptom in symptoms:
-         if symptom in symptom_to_condition:
-              potential_conditions += symptom_to_condition[symptom]
+        if symptom in symptom_to_condition:
+            potential_conditions += symptom_to_condition[symptom]
 
     potential_conditions = list(set(potential_conditions))
-    
+
     if potential_conditions:
-        return render_template('results.html',conditions=potential_conditions)
-    else:
-        flash("Sorry, we could not find any potential conditions based on your symptoms.")
-    return render_template('results.html',conditions=potential_conditions)
+        return render_template('results.html', conditions=potential_conditions)
+
+    return render_template('results.html', conditions=potential_conditions)
